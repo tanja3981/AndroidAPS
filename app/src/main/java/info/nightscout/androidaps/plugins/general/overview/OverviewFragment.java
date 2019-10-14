9a2cb302df7f72d25491f89de9719fec644bb05d
Milos Kozak <m.kozak@sysop.cz>
2019-10-14 20:26:33 +0200

    Events to RxBus

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.java
@@ -916,6 +916,12 @@ public class OverviewFragment extends Fragment implements View.OnClickListener, 
                         FabricPrivacy::logException
                 ));
         disposable.add(RxBus.INSTANCE
+                .toObservable(EventAutosensCalculationFinished.class)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(event -> scheduleUpdateGUI("EventAutosensCalculationFinished"),
+                        FabricPrivacy::logException
+                ));
+        disposable.add(RxBus.INSTANCE
                 .toObservable(EventPumpStatusChanged.class)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(event -> updatePumpStatus(event.getStatus()),
@@ -935,11 +941,6 @@ public class OverviewFragment extends Fragment implements View.OnClickListener, 
     @Subscribe
     public void onStatusEvent(final EventPreferenceChange ev) {
         scheduleUpdateGUI("EventPreferenceChange");
-    }
-
-    @Subscribe
-    public void onStatusEvent(final EventAutosensCalculationFinished ev) {
-        scheduleUpdateGUI("EventAutosensCalculationFinished");
     }
 
     @Subscribe
