a0a42e6313fd771f7ac6d2a66d859509cb6988ac
Andy Rozman <aleksander.rozman@bearingpoint.com>
2019-10-23 11:14:04 +0100

    - Added a lot of logging for Double Bolus problem... - Found one small problem when calling update treatment (if called from NS)...

--- app/src/main/java/info/nightscout/androidaps/plugins/pump/medtronic/data/MedtronicHistoryData.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/pump/medtronic/data/MedtronicHistoryData.java
@@ -83,6 +83,8 @@ public class MedtronicHistoryData {
 
     private long lastIdUsed = 0;
 
+    public static boolean doubleBolusDebug = true;
+
 
     public MedtronicHistoryData() {
         this.allHistory = new ArrayList<>();
@@ -584,18 +586,23 @@ public class MedtronicHistoryData {
 
         long oldestTimestamp = getOldestTimestamp(entryList);
 
+        Gson gson = MedtronicUtil.getGsonInstance();
+
         List<? extends DbObjectBase> entriesFromHistory = getDatabaseEntriesByLastTimestamp(oldestTimestamp, ProcessHistoryRecord.Bolus);
 
-//        LOG.debug(processHistoryRecord.getDescription() + " List (before filter): {}, FromDb={}", gsonPretty.toJson(entryList),
-//                gsonPretty.toJson(entriesFromHistory));
+        if (doubleBolusDebug)
+            LOG.debug("DoubleBolusDebug: List (before filter): {}, FromDb={}", gson.toJson(entryList),
+                    gson.toJson(entriesFromHistory));
 
         filterOutAlreadyAddedEntries(entryList, entriesFromHistory);
 
-        if (entryList.isEmpty())
+        if (entryList.isEmpty()) {
             return;
+        }
 
-//        LOG.debug(processHistoryRecord.getDescription() + " List (after filter): {}, FromDb={}", gsonPretty.toJson(entryList),
-//                gsonPretty.toJson(entriesFromHistory));
+        if (doubleBolusDebug)
+            LOG.debug("DoubleBolusDebug: List (after filter): {}, FromDb={}", gson.toJson(entryList),
+                    gson.toJson(entriesFromHistory));
 
         if (isCollectionEmpty(entriesFromHistory)) {
             for (PumpHistoryEntry treatment : entryList) {
@@ -850,7 +857,10 @@ public class MedtronicHistoryData {
 
         BolusDTO bolusDTO = (BolusDTO) bolus.getDecodedData().get("Object");
 
+
         if (treatment == null) {
+            if (doubleBolusDebug)
+                LOG.debug("DoubleBolusDebug: addBolus(tretament==null): Bolus={}", bolusDTO);
 
             switch (bolusDTO.getBolusType()) {
                 case Normal: {
@@ -862,6 +872,9 @@ public class MedtronicHistoryData {
                     detailedBolusInfo.insulin = bolusDTO.getDeliveredAmount();
 
                     addCarbsFromEstimate(detailedBolusInfo, bolus);
+
+                    if (doubleBolusDebug)
+                        LOG.debug("DoubleBolusDebug: addBolus(tretament==null): DetailedBolusInfo={}", detailedBolusInfo);
 
                     boolean newRecord = TreatmentsPlugin.getPlugin().addToHistoryTreatment(detailedBolusInfo, false);
 
@@ -927,6 +940,9 @@ public class MedtronicHistoryData {
         if (bolus.containsDecodedData("Estimate")) {
 
             BolusWizardDTO bolusWizard = (BolusWizardDTO) bolus.getDecodedData().get("Estimate");
+
+            if (doubleBolusDebug)
+                LOG.debug("DoubleBolusDebug: addCarbsFromEstimate: Bolus={}, BolusWizardDTO={}", bolus, bolusWizard);
 
             detailedBolusInfo.carbs = bolusWizard.carbs;
         }
@@ -1278,21 +1294,25 @@ public class MedtronicHistoryData {
 
         //LocalDateTime oldestEntryTime = null;
 
+        if (doubleBolusDebug)
+            LOG.debug("DoubleBolusDebug: getOldestTimestamp. Oldest entry found: time={}, object={}", dt, currentTreatment);
+
         try {
 
             GregorianCalendar oldestEntryTime = DateTimeUtil.toGregorianCalendar(dt);
+            if (doubleBolusDebug)
+                LOG.debug("DoubleBolusDebug: getOldestTimestamp. oldestEntryTime: {}", DateTimeUtil.toString(oldestEntryTime));
             oldestEntryTime.add(Calendar.MINUTE, -2);
+
+            if (doubleBolusDebug)
+                LOG.debug("DoubleBolusDebug: getOldestTimestamp. oldestEntryTime (-2m): {}, timeInMillis={}", DateTimeUtil.toString(oldestEntryTime), oldestEntryTime.getTimeInMillis());
 
             return oldestEntryTime.getTimeInMillis();
 
-//            if (this.pumpTime.timeDifference < 0) {
-//                oldestEntryTime = oldestEntryTime.plusSeconds(this.pumpTime.timeDifference);
-//            }
         } catch (Exception ex) {
-            LOG.error("Problem decoding date from last record: {}" + currentTreatment);
+            LOG.error("Problem decoding date from last record: {}", currentTreatment);
             return 8; // default return of 6 minutes
         }
-
 
     }
 
