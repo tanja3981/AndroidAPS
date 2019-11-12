394e2955d3fa9b7fcd8a31ed0b09c24229498815
Johannes Mockenhaupt <git@jotomo.de>
2019-11-12 13:26:03 +0100

    Log unhandled exception rather than printing to stdout.

--- app/src/main/java/info/nightscout/androidaps/plugins/pump/medtronic/data/MedtronicHistoryData.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/pump/medtronic/data/MedtronicHistoryData.java
@@ -68,7 +68,6 @@ import info.nightscout.androidaps.utils.SP;
 // All things marked with "TODO: Fix db code" needs to be updated in new 2.5 database code
 
 public class MedtronicHistoryData {
-
     private static final Logger LOG = LoggerFactory.getLogger(L.PUMP);
 
     private List<PumpHistoryEntry> allHistory = null;
@@ -530,7 +529,7 @@ public class MedtronicHistoryData {
             data.put("eventType", event);
             NSUpload.uploadCareportalEntryToNS(data);
         } catch (JSONException e) {
-            e.printStackTrace();
+            LOG.debug("Unhandled exception", e);
         }
     }
 
